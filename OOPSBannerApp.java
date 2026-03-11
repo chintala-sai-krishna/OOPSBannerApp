5bde302ba815372ef3795ca90a61ddde730723cf
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-03-12 01:22:52 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * This program prints OOPS in the console
+ * 
+ * @author Sai
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+}
