5caea3e1d68b6c1ebb493cfeef9a2979589285c1
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-03-12 02:17:16 +0530

    Coded UC6 using static helper functions for banner characters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,61 @@
 /**
  * OOPSBannerApp
- * UC5: Array Initialization with String.join()
- *
+ * UC6: Static Functions for Banner Characters
+ * Generates OOPS banner using helper methods
+ * 
  * @author Sai
- * @version 1.4
+ * @version 1.5
  */
 
 public class OOPSBannerApp {
 
+    // Pattern for letter O
+    public static String[] printO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Pattern for letter P
+    public static String[] printP() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Pattern for letter S
+    public static String[] printS() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*     *", "*     "),
-            String.join("   ", "*     *", "*     *", "*     *", "*     "),
-            String.join("   ", "*     *", " ***** ", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*      ", "*      ", "*     "),
-            String.join("   ", "*     *", "*      ", "*      ", "*     "),
-            String.join("   ", " ***** ", "*      ", " ***** ", " ***** ")
-        };
+        String[] O = printO();
+        String[] P = printP();
+        String[] S = printS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
         }
     }
 }
